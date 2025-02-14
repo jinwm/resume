6d6417e99cea6af5c0237c974d0f53f48e49a99f
fangsiming <jinwming@qq.com>
2025-02-14 19:43:50 +0800

    feat: 文案更新

--- src/views/ResumeIndex.vue
+++ src/views/ResumeIndex.vue
@@ -11,7 +11,7 @@
             </span></div>
         </div>
         <div class="picture">
-          <img src="@/assets/img/picture.jpg">
+          <img v-if="pictureSrc" :src="pictureSrc" alt="头像" />
         </div>
       </div>
       <div class="info-part">
@@ -56,7 +56,7 @@
                 <div class="sub-box">
                   <p class="sub-label">动态数据海报生成</p>
                   <div class="list">
-                    <p class="value">封装drawImage与drawText方法，支持圆角裁剪、图片尺寸自定义、动态位置设置、文字自动换行，通过async/await逐步渲染，最终导出图片。</p>
+                    <p class="value">基于Canvas封装动态渲染模块，支持图片/文字自定义布局与异步渲染，性能提升90%。</p>
                     <p class="value">采用图片预加载与缓存方案，优化海报生成性能，耗时从3.3s降低至0.3s，性能提升90%。</p>
                     <p class="value">开发二维码合成模块，支持动态嵌入用户专属分享标识。</p>
                   </div>
@@ -70,7 +70,7 @@
                   </div>
                 </div>
                 <div class="sub-box">
-                  <p class="sub-label">数据埋点与上报系统</p>
+                  <p class="sub-label">数据埋点与上报模块</p>
                   <div class="list">
                     <p class="value">设计全局埋点上报模块，使用小程序UUID作为用户标识，支持页面显示上报和自定义事件上报。</p>
                     <p class="value">实现登录态依赖管理，通过Promise锁机制避免wx.login和userinfo接口重复调用。</p>
@@ -91,7 +91,7 @@
               <div class="item">
                 <p class="label">项目成果：</p>
                 <div class="list">
-                  <p class="value">成功上线并稳定运行，日均用户互动量超过10万次。</p>
+                  <p class="value">成功上线并稳定运行，日均用户互动量超过10万+。</p>
                   <p class="value">通过技术优化，海报生成时间减少90%，显著提升用户体验。</p>
                   <p class="value">数据埋点系统有效支持了用户行为分析和产品优化决策。</p>
                 </div>
@@ -109,23 +109,23 @@
               <div class="item">
                 <p class="label">技术栈：</p>
                 <div class="list">
-                  <p class="value">Vue2/Vue3+TypeScript、jQuery、Swiper、HTML2Canvas、QRCode.js、Preloader</p>
+                  <p class="value">Vue2、Vue3、TypeScript、jQuery、Swiper、HTML2Canvas、QRCode.js、Preloader</p>
                 </div>
               </div>
               <div class="item">
                 <p class="label">核心贡献：</p>
                 <div class="list">
-                  <p class="value">独立承接需求，年均交付40+个营销页面（PC/移动端/H5活动），单项目平均交付周期3-10天</p>
-                  <p class="value">使用CSS Animation实现常见动画效果，满足多样化设计需求</p>
-                  <p class="value">基于HTML2Canvas+QRCode.js实现分享图生成功能，支持动态嵌入用户二维码</p>
-                  <p class="value">采用Swiper快速搭建轮播组件，配合CSS样式实现设计稿高还原度</p>
+                  <p class="value">独立承接需求，年均交付45+个营销页面（PC/移动端/H5活动），单项目平均交付周期3-10天。</p>
+                  <p class="value">使用CSS Animation实现常见动画效果，满足多样化设计需求。</p>
+                  <p class="value">基于HTML2Canvas+QRCode.js实现分享图生成功能，支持动态嵌入用户二维码。</p>
+                  <p class="value">采用Swiper快速搭建轮播组件，配合CSS样式实现设计稿高还原度。</p>
                 </div>
               </div>
               <div class="item">
                 <p class="label">技术亮点：</p>
                 <div class="list">
-                  <p class="value">建立个人开发流程规范，通过工具复用将相似需求开发效率提升50%</p>
-                  <p class="value">沉淀高频插件配置模板（Swiper/HTML2Canvas等），降低重复配置时间成本</p>
+                  <p class="value">建立个人开发流程规范，通过工具复用将相似需求开发效率提升50%。</p>
+                  <p class="value">沉淀高频插件配置模板（Swiper/HTML2Canvas等），降低重复配置时间成本。</p>
                 </div>
               </div>
             </div>
@@ -153,7 +153,16 @@
         </div>
       </div>
 
-
+      <div class="info-part">
+        <div class="label">教育经历</div>
+        <div class="content content-gzjl">
+          <div class="line-box">
+            <p class="item">浙江工贸职业技术学院</p>
+            <p class="item">软件技术（大专）</p>
+            <p class="item">2019年9月-2022年6月</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <a href="javascript:;" class="btn-export" title="导出图片" @click="handelExportImg">导出图片</a>
@@ -184,6 +193,24 @@ const basicInfo = ref([
 const workExperience = ref([
   ['杭州鑫烨科技有限公司（网易合作供应商）', '文娱部 - 前端开发工程师', '2022年4月 - 至今']
 ])
+
+let pictureSrc = require('@/assets/img/picture.jpg');
+
+const showPrivacy = ref(false);
+
+if (!showPrivacy.value) {
+  pictureSrc = null;
+
+  const basicInfoKeys = ['姓名', '出生年月', '电话', '邮箱'];
+  basicInfo.value = basicInfo.value.map(item => {
+    if (basicInfoKeys.includes(item[0])) {
+      item[1] = '********';
+    }
+    return item;
+  })
+
+  workExperience.value[0][0] = '杭州**科技有限公司（网易合作供应商）';
+}
 
 const handelExportImg = () => {
   if (!resumeContainer.value) {
