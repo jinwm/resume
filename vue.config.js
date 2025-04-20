a02ae3d9212422c61564424e33c5f6664453dc33
fangsiming <jinwming@qq.com>
2025-04-20 22:57:25 +0800

    feat: dosc

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 配置打包结果的资源路径
-  publicPath: './'
+  publicPath: './',
+  // 配置打包后的文件名
+  outputDir: 'docs',
 })
