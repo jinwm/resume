5378c5150d7f1c8b3debc8e920a0669c6d8b174d
fangsiming <jinwming@qq.com>
2026-03-06 00:16:48 +0800

    feat: 调整样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
-  {
-    path: '/=2I3TIONFDBOESAS8hGB72A8JR=-DS89Q2H1NF',
-    name: 'Index',
-    component: () => import(/* webpackChunkName: "index" */ '../views/ResumeIndex.vue'),
-  },
+  // {
+  //   path: '/=2I3TIONFDBOESAS8hGB72A8JR=-DS89Q2H1NF',
+  //   name: 'Index',
+  //   component: () => import(/* webpackChunkName: "index" */ '../views/ResumeIndex.vue'),
+  // },
   {
     path: '/H0-I0I34ONTOIN78SD612bG0RN3240',
     name: 'ResumeV2',
